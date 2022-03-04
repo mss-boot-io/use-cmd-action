3e9a7da9c40a3f47b5d707ddc4c9232554c11bb3
lwnmengjing <991154416@qq.com>
2022-03-04 14:16:38 +0800

    push config

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as process from 'child_process'
+// import * as httpm from '@actions/http-client'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    // const http = new httpm.HttpClient('http-client')
+    // http.get()
+    process.exec('ls', (err, std) => {
+      if (err) {
+        core.error(err)
+        return
+      }
+      core.debug(std)
+    })
+    core.debug(`ok`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
