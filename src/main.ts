23366cb68e82d5331a8ef5b5e2893354a574f07f
lwnmengjing <991154416@qq.com>
2022-03-04 17:05:40 +0800

    support cmd tools

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,47 @@
 import * as core from '@actions/core'
-import * as process from 'child_process'
-// import * as httpm from '@actions/http-client'
+import * as child from 'child_process'
+import * as httpm from '@actions/http-client'
+
+class cmdApp {
+  app?: string
+  source?: string
+}
 
 async function run(): Promise<void> {
+  const app = core.getInput("app")
   try {
-    // const http = new httpm.HttpClient('http-client')
-    // http.get()
-    process.exec('ls', (err, std) => {
+    const http = new httpm.HttpClient('http-client')
+    const {
+      result,
+      statusCode
+    } = await http.getJson<cmdApp[]>('https://mss-boot-io.github.io/use-cmd-action/cmd-list.json')
+    if (statusCode != 200 || result == null) {
+      core.error('source get error')
+    }
+    let source = ''
+    for (const e of result||[]) {
+      if (app == e.app) {
+        source= e.source || ''
+      }
+    }
+    if (source === '') {
+      core.error('source get error')
+    }
+
+    child.exec(`curl -o ${app} ${source}`, (err, std) => {
       if (err) {
         core.error(err)
         return
       }
       core.debug(std)
     })
-    core.debug(`ok`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    child.exec(`./${app}`, (err, std) => {
+      if (err) {
+        core.error(err)
+        return
+      }
+      core.debug(std)
+    })
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
