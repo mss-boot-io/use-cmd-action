c118c35ae3282dae0430c9ccbd5d287091cc354e
lwnmengjing <991154416@qq.com>
2022-03-16 23:11:37 +0800

    ✨ test kubectl install

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,6 +6,7 @@ import {expect, test} from '@jest/globals'
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
   process.env['INPUT_APP'] = 'kubectl'
+  process.env['INPUT_ARGS'] = 'version'
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
   const options: cp.ExecFileSyncOptions = {
